f73eb03f677244e4aea24872d35606bb97796540
Daniil Mikhaylov <www.daniiland@gmail.com>
2024-05-28 00:01:58 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+import "./App.css";
+
+const App: FC = () => {
+  return (
+    <>
+      <span className="logo">
+        <span className="logo__accent-logo">Ether</span>
+        Mess
+      </span>
+    </>
+  );
+};
+
+export default App;
