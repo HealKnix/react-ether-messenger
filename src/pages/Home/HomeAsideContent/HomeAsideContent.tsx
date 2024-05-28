2855b4c4886522b336417274659f8da5ee7a88d2
Daniil Mikhaylov <www.daniiland@gmail.com>
2024-05-28 18:28:01 +0800

    Исправление отображение аватарок v3.0 и убрал пока что тёмную тему

--- src/pages/Home/HomeAsideContent/HomeAsideContent.tsx
+++ src/pages/Home/HomeAsideContent/HomeAsideContent.tsx
@@ -1,6 +1,17 @@
 import { FC } from "react";
 import AvatarText from "../../../components/AvatarText/AvatarText";
 import Card from "../../../components/Card/Card";
+
+import UserAvatar1 from "../../../assets/images/avatars/users/user1.png";
+import UserAvatar2 from "../../../assets/images/avatars/users/user2.png";
+import UserAvatar3 from "../../../assets/images/avatars/users/user3.png";
+import UserAvatar4 from "../../../assets/images/avatars/users/user4.png";
+import UserAvatar5 from "../../../assets/images/avatars/users/user5.png";
+
+import CommunityAvatar1 from "../../../assets/images/avatars/communities/community1.png";
+import CommunityAvatar2 from "../../../assets/images/avatars/communities/community2.png";
+import CommunityAvatar3 from "../../../assets/images/avatars/communities/community3.png";
+
 import "./HomeAsideContent.css";
 
 const HomeAsideContent: FC = () => {
@@ -9,44 +20,44 @@ const HomeAsideContent: FC = () => {
       <div className="aside-content__wrapper">
         <Card title="Возможные друзья">
           <AvatarText
-            img="src/assets/images/avatars/users/user1.png"
+            img={UserAvatar1}
             name="Helena Hills"
             description="@helenahills"
           />
           <AvatarText
-            img="src/assets/images/avatars/users/user2.png"
+            img={UserAvatar2}
             name="Charles Tran"
             description="@charlestran"
           />
           <AvatarText
-            img="src/assets/images/avatars/users/user3.png"
+            img={UserAvatar3}
             name="Oscar Davis"
             description="@oscardavis"
           />
           <AvatarText
-            img="src/assets/images/avatars/users/user4.png"
+            img={UserAvatar4}
             name="Daniel Jay Park"
             description="@danielj"
           />
           <AvatarText
-            img="src/assets/images/avatars/users/user5.png"
+            img={UserAvatar5}
             name="Carlo Rojas"
             description="@carlorojas"
           />
         </Card>
         <Card title="Сообщества, которые могут вам понравиться">
           <AvatarText
-            img="src/assets/images/avatars/communities/community1.png"
+            img={CommunityAvatar1}
             name="Design Enthusiasts"
             description="13.2k участников"
           />
           <AvatarText
-            img="src/assets/images/avatars/communities/community2.png"
+            img={CommunityAvatar2}
             name="Photographers of SF"
             description="2k участников"
           />
           <AvatarText
-            img="src/assets/images/avatars/communities/community3.png"
+            img={CommunityAvatar3}
             name="Marina crew"
             description="125 участников"
           />
