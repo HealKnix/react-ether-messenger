bec26741eca0c93c6ed206e057a4b848f376847c
Daniil Mikhaylov <www.daniiland@gmail.com>
2024-05-29 19:50:31 +0800

    fix date

--- src/utils/getPastTimeString.ts
+++ src/utils/getPastTimeString.ts
@@ -1,24 +1,31 @@
-export const getPastTimeString = (date: Date) => {
-  const differentDate = new Date().getTime() - date.getTime();
+export const getPastTimeString = (date: Date): string => {
+  const now = Date.now();
+  const diffTime = now - date.getTime();
 
-  const SECOND = 1 * 1000;
-  const MINUTE = 1 * 60 * SECOND;
-  const HOURS = 1 * 60 * MINUTE;
-  const DAY = 1 * 24 * HOURS;
-  const MONTH = 1 * 30 * DAY;
-  const YEAR = 1 * 12 * MONTH;
+  const SECOND = 1000;
+  const MINUTE = 60 * SECOND;
+  const HOUR = 60 * MINUTE;
+  const DAY = 24 * HOUR;
+  const MONTH = 30 * DAY;
+  const YEAR = 12 * MONTH;
 
-  if (differentDate > YEAR)
-    return `${Math.floor(differentDate / YEAR)} г. назад`;
-  else if (differentDate > MONTH)
-    return `${Math.floor(differentDate / MONTH)} мес. назад`;
-  else if (differentDate > DAY)
-    return `${Math.floor(differentDate / DAY)} д. назад`;
-  else if (differentDate > HOURS)
-    return `${Math.floor(differentDate / HOURS)} ч. назад`;
-  else if (differentDate > MINUTE)
-    return `${Math.floor(differentDate / MINUTE)} мин. назад`;
-  else if (differentDate > SECOND)
-    return `${Math.floor(differentDate / SECOND)} сек. назад`;
-  else return `только что`;
+  if (diffTime >= YEAR) {
+    return `${Math.floor(diffTime / YEAR)} г. назад`;
+  }
+  if (diffTime >= MONTH) {
+    return `${Math.floor(diffTime / MONTH)} мес. назад`;
+  }
+  if (diffTime >= DAY) {
+    return `${Math.floor(diffTime / DAY)} д. назад`;
+  }
+  if (diffTime >= HOUR) {
+    return `${Math.floor(diffTime / HOUR)} ч. назад`;
+  }
+  if (diffTime >= MINUTE) {
+    return `${Math.floor(diffTime / MINUTE)} мин. назад`;
+  }
+  if (diffTime >= SECOND) {
+    return `${Math.floor(diffTime / SECOND)} сек. назад`;
+  }
+  return 'только что';
 };
