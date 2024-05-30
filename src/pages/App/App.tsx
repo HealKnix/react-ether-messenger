4f4358d913ddeb8d5455c2b7905a409fe90576ff
Daniil Mikhaylov <www.daniiland@gmail.com>
2024-05-30 23:27:26 +0800

    Небольшой рефакторинг App.tsx и вынесение авторизации в хук

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,53 +1,51 @@
-import { FC, useEffect } from 'react';
+import { FC, useMemo } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 import SidebarMenu from '../../components/SidebarMenu/SidebarMenu';
 import LogIn from '../LogIn/LogIn';
 import SignUp from '../SignUp/SignUp';
 
+import { useInitializeAuth } from '../../hooks/useInitializeAuth';
 import { router } from '../../routes';
-import { useAuthStore } from '../../store/useAuthStore';
 
 import './App.scss';
 
 const App: FC = () => {
-  const authStore = useAuthStore();
+  const { isAuthenticated, loading } = useInitializeAuth();
 
-  useEffect(() => {
-    if (localStorage.getItem('user'))
-      authStore.setUser(JSON.parse(localStorage.getItem('user') ?? ''));
-    else authStore.setUser(null);
-  }, []);
-
-  return (
-    <>
-      {!authStore.isAuth() && (
-        <>
-          <Routes>
-            <Route path="*" element={<span>404</span>} />
-            <Route index path="/" element={<LogIn />} />
-            <Route path="/sign-up" element={<SignUp />} />
-          </Routes>
-        </>
-      )}
-      {authStore.isAuth() && (
-        <>
-          <SidebarMenu />
-          <Routes>
-            <Route path="*" element={<span>404</span>} />
-            <Route index path="/" element={<Navigate to="/home" replace />} />
-            {router.map((route) => (
-              <Route
-                path={route.path}
-                element={<route.component />}
-                key={route.path}
-              />
-            ))}
-          </Routes>
-        </>
-      )}
-    </>
+  const authenticatedRoutes = useMemo(
+    () => (
+      <>
+        <SidebarMenu />
+        <Routes>
+          <Route path="*" element={<span>404</span>} />
+          <Route index path="/" element={<Navigate to="/home" replace />} />
+          {router.map((route) => (
+            <Route
+              path={route.path}
+              element={<route.component />}
+              key={route.path}
+            />
+          ))}
+        </Routes>
+      </>
+    ),
+    [router],
   );
+
+  const unauthenticatedRoutes = (
+    <Routes>
+      <Route path="*" element={<Navigate to="/" replace />} />
+      <Route index path="/" element={<LogIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+    </Routes>
+  );
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return <>{isAuthenticated ? authenticatedRoutes : unauthenticatedRoutes}</>;
 };
 
 export default App;
