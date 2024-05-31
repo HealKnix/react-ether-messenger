464a98e1aa036116fd5f7d240894531c4f6e4f13
Daniil Mikhaylov <www.daniiland@gmail.com>
2024-06-01 01:46:51 +0800

    Сделал отдельный стиль под мобильную панель навигации

--- src/components/SidebarMenu/SidebarMenu.tsx
+++ src/components/SidebarMenu/SidebarMenu.tsx
@@ -3,9 +3,14 @@ import './SidebarMenu.scss';
 import { Link, useLocation } from 'react-router-dom';
 import { router } from '@/routes';
 import Logo from '@/components/Logo/Logo';
+import HomeSVG from '../SVG/HomeSVG';
+import FriendsSVG from '../SVG/FriendsSVG';
+import MessageSVG from '../SVG/MessageSVG';
+import { useAuthStore } from '@/store/useAuthStore';
 
 const SidebarMenu: FC = () => {
   const location = useLocation();
+  const authStore = useAuthStore();
 
   return (
     <>
@@ -13,7 +18,7 @@ const SidebarMenu: FC = () => {
         <div className="sticky-menu">
           <Logo />
 
-          <ul className="side-bar-menu__nav">
+          <ul className="side-bar-menu__nav desktop-nav">
             {router
               .filter((route) => route.show)
               .sort((a, b) => a.order - b.order)
@@ -32,6 +37,52 @@ const SidebarMenu: FC = () => {
                   </Link>
                 </li>
               ))}
+          </ul>
+
+          <ul className="bottom-menu__nav mobile-nav">
+            <li>
+              <Link
+                to="/home"
+                className={'/home' === location.pathname ? 'active nav' : 'nav'}
+              >
+                <HomeSVG />
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/friends"
+                className={
+                  '/friends' === location.pathname ? 'active nav' : 'nav'
+                }
+              >
+                <FriendsSVG />
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/messenger"
+                className={
+                  '/messenger' === location.pathname ? 'active nav' : 'nav'
+                }
+              >
+                <MessageSVG />
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/profile"
+                className={
+                  '/profile' === location.pathname ? 'active nav' : 'nav'
+                }
+              >
+                <div
+                  className="avatar-nav"
+                  style={{
+                    backgroundImage: `url(${authStore.user?.avatar})`,
+                  }}
+                ></div>
+              </Link>
+            </li>
           </ul>
         </div>
       </div>
